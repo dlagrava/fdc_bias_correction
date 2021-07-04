cefab60ffede05ad7be1f72a9ccb2e5912071a9e
dlagrava <dlagrava@gmail.com>
2021-07-04 16:41:27 +1200

    Added stub for cross-validation

--- bias_correction/FDC_bias_correction.py
+++ bias_correction/FDC_bias_correction.py
@@ -12,6 +12,13 @@ attrs_raw_simulation = {'description': 'simulation flow values', 'standard_name'
              'valid_max': 1e+20}
 
 
+# plt.plot(flow_values, label="Simulated")
+# plt.plot(bias_corrected_values, label="Bias-corrected")
+# plt.title("Simulated vs. Bias-corrected for {}".format(reach))
+# plt.legend()
+# plt.savefig("{}/{}_bias_correction.png".format(output_dir, reach))
+# plt.close()
+
 def remove_bias_flow(flow_values, simulated_fdc_probs, simulated_fdc, observed_fdc_probs, observed_fdc):
     """
     Perform the algorithm for the bias-correction in 2 vectorized operations.
@@ -75,13 +82,6 @@ def bias_correction_all_time(simulation_ds: xr.Dataset, fdc_ds: xr.Dataset,
         raw_values.append(flow_values)
         bias_corrected.append(bias_corrected_values)
 
-        # plt.plot(flow_values, label="Simulated")
-        # plt.plot(bias_corrected_values, label="Bias-corrected")
-        # plt.title("Simulated vs. Bias-corrected for {}".format(reach))
-        # plt.legend()
-        # plt.savefig("{}/{}_bias_correction.png".format(output_dir, reach))
-        # plt.close()
-
     raw_values = np.array(raw_values).T
     bias_corrected = np.array(bias_corrected).T
 
@@ -95,4 +95,50 @@ def bias_correction_all_time(simulation_ds: xr.Dataset, fdc_ds: xr.Dataset,
     return bias_corrected_dict
 
 def bias_correction_cross_validation(simulation_ds: xr.Dataset, fdc_ds: xr.Dataset, start_year=1972, end_year=2019):
-    pass
+    flow_variable = "mod_streamq"
+    time_slice = slice('%i-01-01' % start_year, '%i-12-31' % end_year)
+    reduced_ds = simulation_ds.sel(time=time_slice)
+
+    simulation_rchid = simulation_ds.rchid.values
+    # Containers for output
+    bias_corrected_reaches = []
+    raw_values = []
+    bias_corrected = []
+
+    time_coord = reduced_ds.time
+    print(time_coord)
+
+    for reach in simulation_rchid:
+        # check if we have valid FDC for simulation and observation
+        try:
+            print(np.where(fdc_ds.rchid.values == reach))
+            print(np.where(fdc_ds.station_rchid.values == reach))
+            simulated_fdc_idx = np.where(fdc_ds.rchid.values == reach)[0][0]
+            observed_fdc_idx = np.where(fdc_ds.station_rchid.values == reach)[0][0]
+        except IndexError as error:
+            print("Error getting simulated/observed FDC, ignoring {}".format(reach))
+            print(error)
+            continue
+
+        flow_values = reduced_ds.variables[flow_variable][:, simulated_fdc_idx, 0, 0].values
+        percentiles = fdc_ds.variables["percentile"][:].values
+        simulated_fdc = fdc_ds.variables["Sim_FDC"][simulated_fdc_idx, :].values
+        observed_fdc = fdc_ds.variables["Obs_FDC"][observed_fdc_idx, :].values
+        bias_corrected_values = remove_bias_flow(flow_values, percentiles, simulated_fdc, percentiles, observed_fdc)
+
+        bias_corrected_reaches.append(reach)
+        raw_values.append(flow_values)
+        bias_corrected.append(bias_corrected_values)
+
+    raw_values = np.array(raw_values).T
+    bias_corrected = np.array(bias_corrected).T
+
+    bias_corrected_dict = {
+        'rchid': {'dims': ('nrch',), 'data': bias_corrected_reaches, 'attrs': attrs_rchid},
+        'bias_corrected': {'dims': ('time', 'nrch'), 'data': bias_corrected, 'attrs': attrs_bias_corrected},
+        'raw_simulation': {'dims': ('time', 'nrch'), 'data': raw_values, 'attrs': attrs_raw_simulation},
+        'time': {'dims': ('time',), 'data': time_coord}
+    }
+
+    return bias_corrected_dict
+
--- bias_correction_simulated.py
+++ bias_correction_simulated.py
@@ -6,12 +6,13 @@ import os
 import numpy as np
 import xarray as xr
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_nc_file", help="netCDF file containing the simulations")
     parser.add_argument("fdc_nc_file", help="netCDF file containing simulated and observed FDC")
     parser.add_argument("-output_dir", default=".")
-    parser.add_argument("-output_file_name", default="bias_corrected_fdc")
+    parser.add_argument("-output_file_name", default="bias_corrected_fdc.nc")
     return parser.parse_args()
 
 
@@ -25,11 +26,11 @@ def main():
     input_ds = xr.open_dataset(input_nc)
     fdc_ds = xr.open_dataset(fdc_nc)
 
-    bias_corrected_dict = bias_correction.FDC_bias_correction.bias_correction_all_time(input_ds, fdc_ds, start_year=1990, end_year=2000)
+    bias_corrected_dict = bias_correction.FDC_bias_correction.bias_correction_all_time(input_ds, fdc_ds,
+                                                                                       start_year=1990, end_year=2000)
 
     output_ds = xr.Dataset.from_dict(bias_corrected_dict)
     output_ds.to_netcdf("{}/{}".format(output_dir, output_file_name))
-
 
 
 if __name__ == "__main__":
