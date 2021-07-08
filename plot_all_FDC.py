abc52eb7ccea1869cfedfbc5476a9216dae88188
dlagrava <dlagrava@gmail.com>
2021-07-08 13:53:12 +1200

    Up to plotting of the bias-correction results

--- plot_all_FDC.py
+++ plot_all_FDC.py
@@ -3,6 +3,7 @@ import xarray as xr
 import numpy as np
 
 import argparse
+import os
 
 
 def parse_args():
@@ -19,6 +20,9 @@ def main():
     fdc_nc = args.fdc_nc
     output_dir = args.output_dir
     fdc_ds = xr.open_dataset(fdc_nc)
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
     rchid = fdc_ds.rchid.values
     station_rchid = np.array([])
@@ -41,19 +45,29 @@ def main():
     for reach in common_reaches:
         idx_rchid = np.where(rchid == reach)[0][0]
         idx_station_rchid = np.where(station_rchid == reach)[0][0]
+        if len(fdc_ds.Sim_FDC.dims) == 2:
+            sim_fdc = fdc_ds.Sim_FDC[idx_rchid, :].values
+        else:
+            sim_fdc = fdc_ds.Sim_FDC[:, idx_rchid, :].values
 
-        sim_fdc = fdc_ds.Sim_FDC[idx_rchid, :].values
         obs_fdc = fdc_ds.Obs_FDC[idx_station_rchid, :].values
 
         plt.figure(figsize=(12,9))
         ax = plt.subplot(111)
         ax.spines["top"].set_visible(False)
         ax.spines["right"].set_visible(False)
-        plt.semilogy(exceedence, sim_fdc, label="Simulated FDC")
+        if len(fdc_ds.Sim_FDC.dims) == 2:
+            plt.semilogy(exceedence, sim_fdc, label="Simulated FDC")
+        else:
+            years = len(fdc_ds.year_removed.values)
+            for year_idx in range(years):
+                plt.semilogy(exceedence, sim_fdc[year_idx, :], label="Simulated FDC ({} removed)".format(fdc_ds.year_removed.values[year_idx]))
+
         if obs_fdc.max() > -9999:
             plt.semilogy(exceedence, obs_fdc, label="Observed FDC")
         plt.legend()
         plt.title(reach)
+
         plt.savefig("{}/FDC_{}.png".format(output_dir, reach))
         plt.close()
 
