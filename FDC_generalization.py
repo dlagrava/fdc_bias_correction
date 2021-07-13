40e076a7c6efb9f406a5839c8736cab1658a4eb0
Daniel Lagrava <dlagrava@gmail.com>
2021-07-13 17:08:00 +1200

    Train and test data is ready. Need to import the models

--- FDC_generalization.py
+++ FDC_generalization.py
@@ -1,29 +1,61 @@
-import regression_models
+#import regression_models
 
 import xarray as xr
 import pandas as pd
 import numpy as np
 import scipy.stats
 from scipy.interpolate import interp1d
+from sklearn.model_selection import train_test_split
 
 import argparse
-import os
+from typing import Sequence, Tuple, Any
 
 
-def resample_fdc(new_probs, original_probs, original_fdc):
+def resample_fdc(new_probs: np.ndarray, original_probs: np.ndarray, original_fdc: np.ndarray):
+    """
+    Given an FDC original_fdc for the exceedences given in original_probs, resample it
+    to new_probs. This uses a spline and will extrapolate if outside the domain.
+
+    Parameters
+    ----------
+    new_probs: np.ndarray
+        exceedance probabilities for the resampled FDC
+    original_probs: np.ndarray
+        original exceedance probabilities of the FDC
+    original_fdc: np.ndarray
+        original FDC values for the original exceedance probabilities.
+
+    Returns
+    -------
+
+    """
     new_fdc = interp1d(original_probs, original_fdc,
                        kind='slinear', fill_value='extrapolate')(new_probs)
     return new_fdc
 
 
 def calculate_parameters_fdc(distribution, original_fdc):
+    """
+    Approximate the
+
+    Parameters
+    ----------
+    distribution
+    original_fdc
+
+    Returns
+    -------
+
+    """
     dist = getattr(scipy.stats, distribution)
     params = dist.fit(original_fdc)
     return params
 
 
 def join_data_fdc(fdc_ds: xr.Dataset, characteristics_df: pd.DataFrame,
-                  method: str, selected_characteristics: list, categorical_characteristics: list=[]) -> pd.DataFrame:
+                  method: str, selected_characteristics: Sequence[str],
+                  categorical_characteristics: Sequence[str] = []) -> Tuple[pd.DataFrame, Sequence[str], Sequence[str]]:
+
     rchids_with_characteristics = characteristics_df.index
     rchids_with_fdc = fdc_ds.station_rchid.values
 
@@ -52,14 +84,15 @@ def join_data_fdc(fdc_ds: xr.Dataset, characteristics_df: pd.DataFrame,
         characteristics_df = characteristics_df.join(hot_encoding)
         selected_characteristics_with_categorical += hot_encoding_cols
 
-    training_df = pd.DataFrame(index=common_reaches, columns=selected_characteristics_with_categorical + list(columns_result))
+    training_df = pd.DataFrame(index=common_reaches,
+                               columns=selected_characteristics_with_categorical + list(columns_result))
 
     for reach in common_reaches:
         characteristics_reach = characteristics_df.loc[reach][selected_characteristics_with_categorical]
         fdc_idx = np.where(fdc_ds.station_rchid.values == reach)[0][0]
         fdc_reach = fdc_ds.Obs_FDC[fdc_idx, :].values
         # rescale the values (minus the categorical)
-        fdc_reach = np.log10(fdc_reach/characteristics_df.loc[reach]["uparea"])
+        fdc_reach = np.log10(fdc_reach / characteristics_df.loc[reach]["uparea"])
 
         if fdc_type == "discrete":
             fdc = resample_fdc(probabilities_fdc, exceedence_rates, fdc_reach)
@@ -71,7 +104,7 @@ def join_data_fdc(fdc_ds: xr.Dataset, characteristics_df: pd.DataFrame,
         training_df.loc[reach, selected_characteristics_with_categorical] = characteristics_reach
         training_df.loc[reach, columns_result] = fdc
 
-    return training_df
+    return training_df, selected_characteristics_with_categorical, columns_result
 
 
 def parse_args():
@@ -98,18 +131,21 @@ def main():
 
     # This is for NIWA data, we need to build CAMELS the same
     selected_characteristics = ["log10_elevation", "usRainDays10", "usPET", "usAnRainVar", "usParticleSize",
-                                         "usAveSlope", "log10_uparea_m2"]
+                                "usAveSlope", "log10_uparea_m2"]
     categorical_characteristics = ["CLIMATE"]
 
-    joint_fdc_characteristics = join_data_fdc(input_fdc_ds, properties_df, method, selected_characteristics, categorical_characteristics)
+    joint_fdc_characteristics, all_characteristics_list, all_results_list = join_data_fdc(input_fdc_ds, properties_df,
+                                                                                          method,
+                                                                                          selected_characteristics,
+                                                                                          categorical_characteristics)
     print(joint_fdc_characteristics)
 
     joint_fdc_characteristics.to_csv("training_data_fdc.csv")
 
     # Prepare the data for the ML
-    
-
-
+    X, Y = joint_fdc_characteristics[all_characteristics_list], joint_fdc_characteristics[all_results_list]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1)
+    print(X_test, Y_test)
 
 
 if __name__ == "__main__":
