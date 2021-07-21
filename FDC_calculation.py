Fixed the calculation of FDC to avoid 0. Added initial ML models

FDC_calculation.py
@@ -13,7 +13,7 @@ attrs_year_removed = {'description': 'year removed from to construct the corresp
 
 
 def select_valid_years(input_flow_ds: xr.Dataset, station,
-                       min_valid_years: int = 6, max_missing_days: int = 30,
+                       min_valid_years: int = 6, max_missing_hours: int = 30 * 24,
                        flow_variable_name: str = "river_flow_rate") -> np.ndarray:
     """
     Take an xarray DataSet and select all the data from flow_variable. Calculate the missing data and
@@ -26,7 +26,7 @@ def select_valid_years(input_flow_ds: xr.Dataset, station,
     station:
         The input station
     min_valid_years
-    max_missing_days
+    max_missing_hours
     flow_variable_name
 
     Returns
@@ -42,23 +42,31 @@ def select_valid_years(input_flow_ds: xr.Dataset, station,
     for year in np.arange(start_year, end_year + 1):
         year_slice = slice('%i-01-01' % year, '%i-12-31' % year)
         yearly_data = input_flow_ds.sel(time=year_slice)[flow_variable_name][:, station].to_masked_array()
-        if len(yearly_data) - yearly_data.count() > max_missing_days:
+        if len(yearly_data) - yearly_data.count() > max_missing_hours:
             continue
         if np.count_nonzero(np.where(yearly_data >= 1e+35)) > 0:
             print("Wrong or missing values on the data for year", year)
             continue
 
-        # Remove the 0 values?
+        # Remove all non-valid numbers
         all_data = yearly_data.data[np.where(yearly_data.mask != True)]
-        # TODO: check what to do with the 0 data
-        all_data[all_data <= 0.] = np.min(all_data) * 0.01  # 1% of the all time minimal value
+
+        if np.max(all_data) == 0.:
+            print("Year full of 0s: ", len(all_data))
+            print(all_data)
+            continue
+
+        # Remove the 0 values?
         # all_data = all_data[all_data > 0.] # remove all 0. data
         valid_data += list(all_data)
         valid_years += 1
 
-    # print("Valid years: %i" % valid_years)
+    valid_data = np.array(valid_data)
+
     if valid_years >= min_valid_years:
-        return np.array(valid_data)
+        # Changing 0s to some valid value for logs
+        valid_data[valid_data <= 0.] = np.min(valid_data[valid_data > 0.]) * 0.01  # 1% of the all time minimal value
+        return valid_data
 
     # If we do not have enough data, we return an empty array
     return np.array([])

FDC_generalization.py
@@ -1,4 +1,4 @@
-#import regression_models
+import regression_models
 
 import xarray as xr
 import pandas as pd
@@ -6,6 +6,7 @@ import numpy as np
 import scipy.stats
 from scipy.interpolate import interp1d
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 import argparse
 from typing import Sequence, Tuple, Any
@@ -124,6 +125,7 @@ def main():
 
     input_fdc_ds = xr.open_dataset(input_fdc_nc)
     properties_df = pd.read_csv(properties_csv, index_col="rchid")
+    model = method.split("_")[0]
 
     filters = {'GoodSites': True, 'IsWeir': False}
     for k, v in filters.items():
@@ -146,6 +148,17 @@ def main():
     X, Y = joint_fdc_characteristics[all_characteristics_list], joint_fdc_characteristics[all_results_list]
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1)
     print(X_test, Y_test)
+    if model == "rf":
+        regressor = regression_models.create_random_forest_model()
+        regressor.fit(X_train, Y_train)
+    else:
+        regressor = regression_models.create_singleoutputsingle_model(all_characteristics_list, all_results_list)
+        regressor.fit(X_train, Y_train, batch_size=32, epochs=50, verbose=0)
+
+    Y_pred = regressor.predict(X_test)
+    print(Y_pred, Y_test)
+    score = np.sqrt(mean_squared_error(Y_test, Y_pred))
+    print("MSE score: ", score)
 
 
 if __name__ == "__main__":